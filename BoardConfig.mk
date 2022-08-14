08fb3752dab330e32fc085a4d5c65d146046e7e3
Han Sol Jin <hansol@hansol.ca>
2022-08-14 13:18:04 -0700

    gtowifi: setup BoardConfig
    Stuff mostly taken from gts4lv:
    https://github.com/LineageOS/android_device_samsung_gts4lv-common/blob/5663e9e57952fb1db239a765baaf59d2cae3e2a4/BoardConfigCommon.mk
    
    This device uses msm8937 blobs, at least after some looking through
    the vendor files. So set it as such.
    
    Change-Id: I2cae26bb4ea3c02fafb6ae80d55e0b5964feaadc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,12 +1,94 @@
-DEVICE_PATH := device/samsung/gtowifi
-BOARD_VENDOR := samsung
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# Security patch level
-VENDOR_SECURITY_PATCH := 2019-05-01
+BUILD_BROKEN_DUP_RULES := true
+
+DEVICE_PATH := device/samsung/gtowifi
+
+# Platform
+BOARD_VENDOR := samsung
+TARGET_BOARD_PLATFORM := msm8937
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno504
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a53
+
+# Board
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := msm8937
+
+# Camera
+TARGET_USES_QTI_CAMERA_DEVICE := true
+
+# Graphics
+TARGET_USES_GRALLOC1 := true
+TARGET_USES_HWC2 := true
+TARGET_USES_ION := true
+TARGET_USES_ALIGNED_YCBCR_HEIGHT := true
+TARGET_USES_YCRCB_CAMERA_PREVIEW := true
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/samsung/sdm429
+BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE += androidboot.bootdevice=7824900.sdhci earlycon=msm_serial_dm,0x78B0000 androidboot.usbconfigfs=true loop.max_part=7
+
+# Partitions
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+TARGET_COPY_OUT_VENDOR := vendor
+
+# QCOM
+BOARD_USES_QCOM_HARDWARE := true
+
+# Recovery
+BOARD_HAS_DOWNLOAD_MODE := true
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2019-05-01
+
+# SELinux
+include device/qcom/sepolicy-legacy-um/SEPolicy.mk
+
+# Vendor files
 include vendor/samsung/gtowifi/BoardConfigVendor.mk
+
+# Wifi
+BOARD_HAS_QCOM_WLAN := true
+BOARD_HAS_QCOM_WLAN_SDK := true
+BOARD_WLAN_DEVICE := qcwcn
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_DRIVER_FW_PATH_STA := "sta"
+WIFI_DRIVER_FW_PATH_AP  := "ap"
+WIFI_DRIVER_FW_PATH_P2P := "p2p"
+WIFI_DRIVER_OPERSTATE_PATH := "/sys/class/net/wlan0/operstate"
+WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
+WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
+WPA_SUPPLICANT_VERSION := VER_0_8_X
