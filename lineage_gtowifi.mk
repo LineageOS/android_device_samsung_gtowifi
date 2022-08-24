582938332799e105e28c71267c9ae8d8931f5a64
Tim Zimmermann <tim@linux4.de>
2022-08-24 15:40:22 +0200

    gtowifi: Inherit non_ab_device.mk
    * This is required to build applypatch for updating recovery
    
    Change-Id: I38053691f7724b5d6c30159c410d5d04ac136180

--- lineage_gtowifi.mk
+++ lineage_gtowifi.mk
@@ -17,6 +17,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 # Inherit from gtowifi device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
