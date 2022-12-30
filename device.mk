46ef60424da29315bdee117b7324354d2e3187f6
Han Sol Jin <hansol@hansol.ca>
2022-12-30 14:21:43 -0800

    gtowifi: build OSS btconfigstore
    Remove unused btconfigstore impl while at it
    
    Change-Id: I773a939ff95ab7d71e738b395233326b0cb30720

--- device.mk
+++ device.mk
@@ -53,7 +53,9 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.1.vendor
+    android.hardware.bluetooth@1.1.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
