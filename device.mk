6e0ad7a223a596e8c3bca25351b3e69f40086bc5
Han Sol Jin <hansol@hansol.ca>
2022-12-30 14:23:54 -0800

    gtowifi: build OSS android.hardware.bluetooth.audio@2.0-impl
    Change-Id: If25bb2b6945111979846cafe19409f11501bc7ef

--- device.mk
+++ device.mk
@@ -54,6 +54,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
+    android.hardware.bluetooth.audio@2.0-impl \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
