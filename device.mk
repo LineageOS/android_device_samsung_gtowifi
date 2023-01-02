4b7dc07118e0137ba5b30f9ea6e0a244caee9e47
Han Sol Jin <hansol@hansol.ca>
2023-01-01 22:12:29 -0800

    gtowifi: explicitly disable network functionality
    It's a Wi-Fi only tablet, we don't need or want this.
    
    GPS is *now* working.
    
    Change-Id: I374c21c722f4652226982e2decba4571ae9a579f

--- device.mk
+++ device.mk
@@ -188,7 +188,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2019-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
-    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
+    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/aosp_excluded_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/aosp_excluded_hardware.xml 
 
 # Power
 PRODUCT_PACKAGES += \
