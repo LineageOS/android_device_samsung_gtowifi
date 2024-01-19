be40497309c74cfb40bfd8028ec225a3bb313e25
Han Sol Jin <hansol@hansol.ca>
2024-01-19 08:33:23 -0800

    gtowifi: build custom camera provider
    Change-Id: I461f572fe3363ce4e7252da25634121ddb3b827b

--- device.mk
+++ device.mk
@@ -78,8 +78,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.4-impl.gto \
+    android.hardware.camera.provider@2.4-service.gto \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 # Capability Configstore
