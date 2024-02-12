a2cd6a43acb314989fc741eac4630f612f165efa
Han Sol Jin <hansol@hansol.ca>
2024-02-12 12:45:17 -0800

    gtowifi: setup AAPT configs
    Change-Id: I7e216ab047af0486aea07e76ddcf2b2b12f82853

--- device.mk
+++ device.mk
@@ -6,6 +6,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 800
