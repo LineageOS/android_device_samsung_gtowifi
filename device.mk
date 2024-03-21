0ad75069f4f110081936519b282804cc52a98f50
Yumi Yukimura <me.cafebabe@gmail.com>
2024-03-21 23:55:45 +0800

    gtowifi: Switch to USB 1.3-basic HAL
    Change-Id: I6f477db2220b3563b44bb9545b7889166b8a6b06

--- device.mk
+++ device.mk
@@ -332,7 +332,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.3-service.basic
 
 # Vibrator
 PRODUCT_PACKAGES += \
