3a8bfa5dab8967cceee23e347b5785f7a2246c65
Michael Bestas <mikeioannina@gmail.com>
2024-06-15 23:43:37 +0300

    gtowifi: Add a device specific FCM
    Change-Id: I9a36e9b33bf8c73cc0a98d441b141c3c698bc955

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,6 +111,7 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
