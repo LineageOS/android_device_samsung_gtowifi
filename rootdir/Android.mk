8d0033f342bde93e91943e6b62d52817bdf156c8
Han Sol Jin <hansol@hansol.ca>
2024-07-16 16:57:47 -0700

    gtowifi: replace macloader with a shell script
    This shell script will copy the MAC address found in EFS to the kernel
    driver's sysfs. This allows to use the correct MAC address without
    macloader.
    
    The actual MAC address appears to exist in sec_efs; regular efs
    partition will be used as a fallback if it does not exist in sec_efs.
    
    As a side note, macloader itself never actually worked on gtowifi, as
    Samsung never released the source code for the WLAN driver to actually
    make use of it.
    
    Change-Id: I51d9b0bfab5032c9a6c321b3e86c48541551af1d

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -98,6 +98,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := macloader.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := bin/macloader.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.factory.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
