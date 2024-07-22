195dada41de933af3e1ef3e0c9167dc423387dbc
Han Sol Jin <hansol@hansol.ca>
2024-07-22 01:00:22 -0700

    gtowifi: Switch to open-source camera HAL
    Change-Id: Ie725c17f30236716b407b2b7d1df9a0b7aed1d1a

--- device.mk
+++ device.mk
@@ -68,12 +68,18 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Camera
+#PRODUCT_PACKAGES += \
+#    android.frameworks.displayservice@1.0.vendor \
+#    android.frameworks.sensorservice@1.0.vendor \
+#    vendor.qti.hardware.camera.device@1.0.vendor \
+#    android.hardware.camera.provider@2.5-legacy.samsung \
+#    android.hardware.camera.provider@2.5-service.samsung
+
 PRODUCT_PACKAGES += \
-    android.frameworks.displayservice@1.0.vendor \
-    android.frameworks.sensorservice@1.0.vendor \
-    vendor.qti.hardware.camera.device@1.0.vendor \
-    android.hardware.camera.provider@2.5-legacy.samsung \
-    android.hardware.camera.provider@2.5-service.samsung
+    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.4-impl \
+    camera.msm8937 \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 # Capability Configstore
 PRODUCT_PACKAGES += \
