082dfc1a4a90f7fe47b2bc464520f6e9c7a13430
LuK1337 <priv.luk@gmail.com>
2024-11-14 01:40:31 +0000

    gtowifi: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_gtowifi.mk
+++ lineage_gtowifi.mk
@@ -39,7 +39,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 TARGET_VENDOR := samsung
 TARGET_VENDOR_PRODUCT_NAME := gtowifi
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="gtowifixx-user 11 RP1A.200720.012 T290XXU3CVG3 release-keys"
 
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := samsung/gtowifixx/gtowifi:11/RP1A.200720.012/T290XXU3CVG3:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="gtowifixx-user 11 RP1A.200720.012 T290XXU3CVG3 release-keys" \
+    BuildFingerprint="samsung/gtowifixx/gtowifi:11/RP1A.200720.012/T290XXU3CVG3:user/release-keys"
